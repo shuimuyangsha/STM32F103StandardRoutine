58871d4ccb9ab3b6e5fb79a40c6f318887349001
DESKTOP-3UH92KL\XiaohuiDong <1023178387@QQ.com>
2020-12-26 13:20:54 +0800

    ~

--- Experiment_Other/XY_ScatterDiagram/USER/main.c
+++ Experiment_Other/XY_ScatterDiagram/USER/main.c
@@ -5,10 +5,11 @@
 #include "extreme3d.h"
 #include "math.h"
 
-#define NUM_POINT 500
+#define NUM_POINT 50
 float debugX[NUM_POINT];
 float debugY[NUM_POINT];
-
+float debugX1;
+float debugY1;
 int i;
 
 float x;
@@ -19,20 +20,38 @@ float step = 0;
 	LED_Init();		  	//初始化与LED连接的硬件接口
 	while(1)
 	{
-		for (i = 0.1,x=0; i < NUM_POINT;i++,x += 0.1) {
-			//debugX[i] = i;
+		for (i = 0.1,x=0.1; i < 50;i++,x += 0.1) {
+			debugX[i] = i;
 
-			//debugY[i] = i;
+			debugY[i] = i;
 
-			debugX[i] = x;
-
-			debugY[i] = 5 * sin(x);
+			debugX1 = x;
+			
+			debugY1 = x;
+			delay_ms(100);
 		}
 
-		//step += 0.2;
-		//if (step > 10) { step = 0; }
-		//delay_ms(100);
+		step += 1;
+		if (step > 8) { step = 0; }
 
+		delay_ms(1000);
+		delay_ms(1000);
+		delay_ms(1000);
+		delay_ms(1000);
+		delay_ms(1000);
+		delay_ms(1000);
+
+		/*************绘制出正弦函数的散点图*/
+		//for (i = 0.1, x = 0; i < NUM_POINT; i++, x += 0.1) {
+		//	//debugX[i] = i;
+
+		//	//debugY[i] = i;
+
+		//	debugX[i] = x;
+
+		//	debugY[i] = 5 * sin(x);
+		//}
+		/**************绘制出正弦函数的散点图*/
 
 		//debugX[0] = debugY[0] = 10;
 		//debugX[1] = debugY[1] = 20;
